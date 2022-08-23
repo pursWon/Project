1cd221243ebbac41a7772e7c152dddf2e37f8783
Wongi <hpurs95@gmail.com>
2022-08-23 21:08:16 +0900

    if let을 guard let으로 변경

--- CalculateProject/CalculateProject/ViewController.swift
+++ CalculateProject/CalculateProject/ViewController.swift
@@ -52,44 +52,46 @@ class ViewController: UIViewController {
         let number1: Double? = Double(numberTextField1.text!)
         let number2: Double? = Double(numberTextField2.text!)
         
-        // if let을 통해 number1을 안전하게 가져와서 firstNumber라는 상수에 담아준다.
-        // if let을 통해 number2도 안전하게 가져와서 secondNumber라는 상수에 담아준다.
+        // guard let을 통해 number1을 안전하게 가져와서 firstNumber라는 상수에 담아준다.
+        // guard let을 통해 number2도 안전하게 가져와서 secondNumber라는 상수에 담아준다.
         // 안전하게 가져오는 것을 성공한 경우, 계산하는 코드를 넣어준다.
         // 안전하게 가져오는 것을 실패한 경우, 계산 대신에 "이곳엔 문자를 넣을 수 없습니다." 라고 resultLabel.text에 대입해준다.
-        if let firstNumber = number1, let secondNumber = number2 {
-            let plus: Double = firstNumber + secondNumber
-            let plusResult: String = String(plus)
-            
-            if plusButton.backgroundColor == .systemCyan {
-                resultLabel.text = plusResult
-            }
-    
-            let minus: Double = firstNumber - secondNumber
-            let minusResult: String = String(minus)
-            
-            if minusButton.backgroundColor == .systemCyan {
-                resultLabel.text = minusResult
-            }
-            
-            let multiply: Double = firstNumber * secondNumber
-            let multiplyResult: String = String(multiply)
-            
-            if multiplyButton.backgroundColor == .systemCyan {
-                resultLabel.text = multiplyResult
-            }
-            
-            let divide: Double = firstNumber / secondNumber
-            let divideResult: String = String(divide)
-            
-            if divideButton.backgroundColor == .systemCyan {
-                resultLabel.text = divideResult
-                
-                if number2 == 0 {
-                    resultLabel.text = "0으로 나눗셈을 하는 것은 불가능합니다."
-                }
-            }
-        } else {
+        
+        guard let firstNumber = number1, let secondNumber = number2 else {
             resultLabel.text = "이곳엔 문자를 넣을 수 없습니다."
+            return
+        }
+        
+        let plus: Double = firstNumber + secondNumber
+        let plusResult: String = String(plus)
+        
+        if plusButton.backgroundColor == .systemCyan {
+            resultLabel.text = plusResult
+        }
+        
+        let minus: Double = firstNumber - secondNumber
+        let minusResult: String = String(minus)
+        
+        if minusButton.backgroundColor == .systemCyan {
+            resultLabel.text = minusResult
+        }
+        
+        let multiply: Double = firstNumber * secondNumber
+        let multiplyResult: String = String(multiply)
+        
+        if multiplyButton.backgroundColor == .systemCyan {
+            resultLabel.text = multiplyResult
+        }
+        
+        let divide: Double = firstNumber / secondNumber
+        let divideResult: String = String(divide)
+        
+        if divideButton.backgroundColor == .systemCyan {
+            resultLabel.text = divideResult
+            
+            if number2 == 0 {
+                resultLabel.text = "0으로 나눗셈을 하는 것은 불가능합니다."
+            }
         }
     }
     
@@ -103,4 +105,5 @@ class ViewController: UIViewController {
         multiplyButton.backgroundColor = .lightGray
         divideButton.backgroundColor = .lightGray
     }
+    
 }
