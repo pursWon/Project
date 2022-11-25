ba6b69089bcb9bf6f59be80904b96c662ea79fcd
Wongi <hpurs95@gmail.com>
2022-11-25 17:28:06 +0900

    delegate 연습

---
+++ DelegateEx (2)/DelegateEx (2)/Sources/SecondViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class SecondViewController: UIViewController {
+// MARK: - Properties
+
+// MARK: - View LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
---
+++ DelegateEx/DelegateEx/Sources/SecondViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+protocol SampleProtocol {
+    func sendData(data: String)
+}
+
+class SecondViewController: UIViewController {
+    // MARK: Properties
+    var delegate: SampleProtocol?
+    
+    // MARK: UI
+    @IBOutlet weak var textField: UITextField!
+    
+    // MARK: View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func goBackButtonClicked(_ sender: UIButton) {
+    dismiss(animated: true)
+    }
+    
+    @IBAction func saveButtonClicked(_ sender: UIButton) {
+        if textField.text?.isEmpty == false {
+            if let text = textField.text {
+                delegate?.sendData(data: text)
+            }
+            dismiss(animated: true)
+        }
+    }
+}
