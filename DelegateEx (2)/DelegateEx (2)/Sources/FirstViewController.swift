113ae6fabfe579f5d4d7027525949d7b767e7c47
Wongi <hpurs95@gmail.com>
2022-11-25 18:03:31 +0900

    delegate 연습

--- DelegateEx (2)/DelegateEx (2)/Sources/FirstViewController.swift
+++ DelegateEx (2)/DelegateEx (2)/Sources/FirstViewController.swift
@@ -1,10 +1,27 @@
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, SampleProtocol{
+    // MARK: UI
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
     }
     
+    func sendData(data: String) {
+        resultLabel.text = data
+    }
+    
+    // MARK: Actions
+    @IBAction func changeButtonClicked(_ sender: UIButton) {
+        guard let secondVC = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController")
+        as? SecondViewController else { return }
+        secondVC.delegate = self
+        
+        present(secondVC, animated: true)
+    }
 }
+
+
